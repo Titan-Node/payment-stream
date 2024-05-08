47817842452f47f6a66569af5e7267caad264e84
Titan Node <hello@titan-node.com>
2024-05-08 15:42:40 -0700

    Added changes from Marco Audit Report

--- src/PaymentStream.sol
+++ src/PaymentStream.sol
@@ -1,29 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.25;
 
-import { UD60x18, ud } from "@prb/math/src/UD60x18.sol";
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract PaymentStream {
+    event Claimed(uint256 claimedAmount, uint256 remainingAmount);
+    event TerminationInitiated(address initiator);
+    event Terminated(uint256 remainingAmount);
+
+    using SafeERC20 for IERC20;
+
     // Init
-    address public payee;
-    uint256 public duration;
+    address public immutable payee;
+    uint256 public immutable duration;
 
     // Payment
-    address public paymentToken;
-    uint256 public paymentAmount;
+    address public immutable paymentToken;
+    uint256 public immutable paymentAmount;
 
-    uint256 public startTime;
-    uint256 public endTime;
+    uint256 public immutable startTime;
+    uint256 public immutable endTime;
     uint256 public claimedAmount;
 
     // Termination
-    mapping(address => bool) public isTermSigner;
-    address public termReceiver;
-    bool public isTerminated;
+    address public immutable termReceiver;
 
+    struct TermSigner {
+        bool isSigner;
+        bool hasConfirmed; // Ensure fair confirmations
+    }
+
+    mapping(address => TermSigner) public termSigners;
+    bool public isTerminated;
     uint256 public termConfirmations; // 2 required
-    mapping(address => bool) public _hasConfirmed; // Ensure fair confirmations
 
     constructor(
         address _payee,
@@ -33,6 +43,11 @@ contract PaymentStream {
         address[2] memory _termSigners,
         address _termReceiver
     ) {
+        require(
+            _payee != address(0) && _termSigners[0] != address(0) && _termSigners[1] != address(0)
+                && _termReceiver != address(0),
+            "Invalid args"
+        );
         require(_duration > 0 && _paymentToken != address(0) && _paymentAmount > 0, "Invalid args");
 
         payee = _payee;
@@ -42,9 +57,9 @@ contract PaymentStream {
         paymentAmount = _paymentAmount;
 
         // Set Term Signers
-        isTermSigner[_termSigners[0]] = true;
-        isTermSigner[_termSigners[1]] = true;
-        isTermSigner[_payee] = true;
+        termSigners[_termSigners[0]] = TermSigner(true, false);
+        termSigners[_termSigners[1]] = TermSigner(true, false);
+        termSigners[_payee] = TermSigner(true, false);
 
         termReceiver = _termReceiver;
 
@@ -56,7 +71,7 @@ contract PaymentStream {
         uint256 currentTime = block.timestamp > endTime ? endTime : block.timestamp;
         uint256 timeElapsed = currentTime - startTime;
 
-        claimable = (ud(paymentAmount).mul(ud(timeElapsed).div(ud(duration)))).intoUint256() - claimedAmount;
+        claimable = (paymentAmount * timeElapsed / duration) - claimedAmount;
     }
 
     function claim() external {
@@ -68,28 +83,34 @@ contract PaymentStream {
 
         claimedAmount += claimable;
 
-        IERC20(paymentToken).transfer(payee, claimable);
+        IERC20(paymentToken).safeTransfer(payee, claimable);
+
+        emit Claimed(claimable, paymentAmount - claimedAmount);
     }
 
     function terminate() external {
-        require(isTermSigner[msg.sender], "Not authorized");
-        require(!_hasConfirmed[msg.sender], "Already confirmed termination");
+        require(termSigners[msg.sender].isSigner, "Not authorized");
+        require(!termSigners[msg.sender].hasConfirmed, "Already confirmed termination");
         require(claimedAmount < paymentAmount, "All tokens have been claimed");
         require(!isTerminated, "Stream terminated");
 
         if (termConfirmations == 1) {
             // Proceed with termination
             uint256 remainingTokens = IERC20(paymentToken).balanceOf(address(this));
-            
+
             isTerminated = true;
-            _hasConfirmed[msg.sender] = true;
+            termSigners[msg.sender].hasConfirmed = true;
             termConfirmations++;
 
             IERC20(paymentToken).transfer(termReceiver, remainingTokens);
+
+            emit Terminated(remainingTokens);
         } else {
             // First termination call
-            _hasConfirmed[msg.sender] = true;
+            termSigners[msg.sender].hasConfirmed = true;
             termConfirmations++;
+
+            emit TerminationInitiated(msg.sender);
         }
     }
 }
